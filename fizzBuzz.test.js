2a75236f30dd1a2ec3c0a32ef1162518fa03817f
Dilip Valiya <dilip.valiya@thoughtworks.com>
2024-04-29 11:37:31 +0530

    Add functionality for number divisible by 3

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -4,4 +4,8 @@ describe("fizzBuzz", () => {
   test("fizzBuzz function exist", () => {
     fizzBuzz();
   });
+  test("should return fizz if number is divisible by 3", () => {
+    const result = fizzBuzz(6);
+    expect(result).toBe("fizz");
+  });
 });
